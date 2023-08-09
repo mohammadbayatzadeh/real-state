9b43daf791f41665c3b089cd4734838258261b63
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-08-09 17:45:14 +0330

    added loading

--- src/components/templates/RegisterPage.js
+++ src/components/templates/RegisterPage.js
@@ -3,13 +3,19 @@ import { useState } from "react";
 import styles from "./Loginpage.module.css";
 import Link from "next/link";
 import Toast from "../elements/Toast";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { ThreeDots } from "react-loader-spinner";
 
 function LoginPage() {
+  const router = useRouter();
+
   const [form, setForm] = useState({
     email: "",
     password: "",
     configPassword: "",
   });
+  const [loading, setLoading] = useState(false);
 
   const changeHandler = (e) => {
     setForm({
@@ -17,10 +23,25 @@ function LoginPage() {
       [e.target.name]: e.target.value,
     });
   };
-  const submit = () => {
+  const submit = async () => {
     if (form.password !== form.configPassword) {
-      Toast("Passwords not match!", "error");
+      return Toast("پسورد ها مطابقت ندارند!", "error");
     }
+    setLoading(true);
+    await axios
+      .post("/api/auth/signup", {
+        email: form.email,
+        password: form.password,
+      })
+      .then((res) => {
+        setLoading(false);
+        Toast(res.data.message, "success");
+        router.push("/login");
+      })
+      .catch((err) => {
+        setLoading(false);
+        Toast(err.response.data.error, "error");
+      });
   };
   return (
     <div className={styles.form}>
@@ -46,7 +67,19 @@ function LoginPage() {
         type="password"
         onChange={(e) => changeHandler(e)}
       />
-      <button onClick={submit}>ورود</button>
+      {loading ? (
+        <ThreeDots
+          height="55"
+          width="80"
+          radius="9"
+          color="#465293"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{ margin: " 0 auto" }}
+          visible={loading}
+        />
+      ) : (
+        <button onClick={submit}>ورود</button>
+      )}
       <p>
         آیا حساب دارید؟ <Link href="/login">ورود</Link>
       </p>
