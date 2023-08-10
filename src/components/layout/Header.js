fe97e45e32739c18c93e6fe02f9d3ef16c665000
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-08-10 16:47:13 +0330

    header authorization

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,22 @@
+"use client";
+
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
 //icons
-import { VscSignIn } from "react-icons/vsc";
+import { VscSignIn, VscSignOut, VscSymbolProperty } from "react-icons/vsc";
 
 //styles
 import styles from "./Header.module.css";
 
 function Header() {
+  const { status } = useSession();
+  const pathname = usePathname();
+
+  const signoutHandler = async () => {
+    signOut();
+  };
   return (
     <header className={styles.header}>
       <div>
@@ -19,11 +29,36 @@ function Header() {
           </li>
         </ul>
       </div>
-      <div>
-        <Link href="/login" className={styles.signup}>
-          <VscSignIn />
-          <span>ورود</span>
-        </Link>
+      <div className={styles.buttons}>
+        {status === "authenticated" && (
+          <>
+            {pathname !== "/dashboard" && (
+              <Link href="/dashboard" className={styles.button}>
+                <VscSymbolProperty />
+              </Link>
+            )}
+            <button onClick={signoutHandler} className={styles.button}>
+              <VscSignOut />
+              <span>خروج</span>
+            </button>
+          </>
+        )}
+        {status === "unauthenticated" && (
+          <>
+            {pathname !== "/login" && (
+              <Link href="/login" className={styles.button}>
+                <VscSignIn />
+                <span>ورود</span>
+              </Link>
+            )}
+            {pathname !== "/register" && (
+              <Link href="/register" className={styles.button}>
+                <VscSignIn />
+                <span>ثبت نام</span>
+              </Link>
+            )}
+          </>
+        )}
       </div>
     </header>
   );
