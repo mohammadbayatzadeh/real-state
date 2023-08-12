b81001762950ec85a57fa4b4fa947eb8d5fc592b
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-08-12 18:23:16 +0330

    dashboard page designed

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,14 @@
-function Dahboard() {
-  return <div>Dahboard</div>;
+import DashboardPage from "@/components/templates/DashboardPage";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import Boss from "@/models/Boss";
+
+async function Dahboard() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+  const user = await Boss.findOne({ email: session.user.email });
+  return <DashboardPage date={user.createdAt} />;
 }
 
 export default Dahboard;
