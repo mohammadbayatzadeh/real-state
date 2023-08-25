05629777d18bf09c21d322a1f58bfcde7f619e82
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-08-25 17:04:07 +0330

    residentials design completed

--- src/components/templates/ResidentialsPage.js
+++ src/components/templates/ResidentialsPage.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
 import Card from "../elements/Card";
 import styles from "./ResidentialsPage.module.css";
 
 function ResidentialsPage({ data }) {
+  const categories = ["همه", "ویلایی", "آپارتمان", "مغازه", "دفتر"];
   if (data.error)
     return (
       <h3 className={styles.error}> مشکلی در دریافت اطلاعات پیش آمده است </h3>
@@ -9,9 +11,18 @@ function ResidentialsPage({ data }) {
 
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}></div>
+      <div className={styles.sidebar}>
+        <p>دسته بندی</p>
+        <ul>
+          {categories.map((item, index) => (
+            <li key={index}>
+              <Link href='/'>{item}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className={styles.main}>
-        {data.data  .length ? (
+        {data.data.length ? (
           data.data.map((profile) => <Card key={profile._id} data={profile} />)
         ) : (
           <h3 className={styles.error}>هیچ آگهی ثبت نشده است</h3>
