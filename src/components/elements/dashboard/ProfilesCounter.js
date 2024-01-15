69fb929f7eb2fcfe206de801c51660bbdc68c605
mohammadbayatzadeh <mamadbayat777@gmail.com>
2024-01-15 14:18:40 +0330

    profiles conunter added to main dashboard page

--- src/components/elements/dashboard/ProfilesCounter.js
+++
@@ -1,5 +0,0 @@
-async function ProfilesCounter() {
-  return <div>ProfilesCounter</div>;
-}
-
-export default ProfilesCounter;
--- src/components/templates/dashboard/DashboardPage.js
+++ src/components/templates/dashboard/DashboardPage.js
@@ -1,17 +1,27 @@
 //styles
-import ProfilesCounter from "@/components/elements/dashboard/ProfilesCounter";
+import { useContext } from "react";
 import styles from "./DashboardPage.module.css";
+import { UserContext } from "@/components/layout/dashboard/DashboardSideBar";
 
 function DashboardPage({ date }) {
+  const { profiles } = useContext(UserContext);
   return (
     <div className={styles.container}>
       <h3>سلام 👋</h3>
       <p>آگهی های خود را ثبت کنید تا هزاران نفر آن را مشاهده کنند</p>
-      <div className={styles.createdAt}>
-        <p>تاریخ عضویت:</p>
-        <span>{new Date(date).toLocaleDateString("fa-IR")}</span>
+      <div className={styles.data}>
+        <span className={styles.row}>
+          <p>تاریخ عضویت:</p>
+          <span>{new Date(date).toLocaleDateString("fa-IR")}</span>
+        </span>
+        <span>
+          {profiles.length === 0 ? (
+            <p>شما هنوز هیچ آگهی ای ثبت نگرده اید</p>
+          ) : (
+            <p>تعداد آگهی های شما : {profiles.length}</p>
+          )}
+        </span>
       </div>
-      <ProfilesCounter />
     </div>
   );
 }
