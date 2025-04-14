b57b16d2f0039e0a45e12c936de6ac5a85d101f2
mohammadbayatzadeh <91955329+mohammadbayatzadeh@users.noreply.github.com>
2025-04-14 13:18:28 +0330

    header footer

--- src/components/layout/landing/Header.js
+++ src/components/layout/landing/Header.js
@@ -5,7 +5,6 @@ import { usePathname, useRouter } from "next/navigation";
 import { GiArchiveRegister } from "react-icons/gi";
 import { IoDocumentsOutline } from "react-icons/io5";
 import { VscSignIn, VscSignOut, VscSymbolProperty } from "react-icons/vsc";
-import styles from "./Header.module.css";
 
 function Header() {
   const { status } = useSession();
@@ -18,45 +17,60 @@ function Header() {
   };
 
   return (
-    <header className="bg-first w-full h-14 flex items-center justify-between text-third rounded-xl p-4">
-      <div className='flex'>
-        <ul className="flex items-center">
+    <header className="bg-first w-full h-14 flex items-center justify-between text-third rounded-xl p-4 mb-12">
+      <div className="flex">
+        <ul className="flex items-center gap-2">
           <li>
             <Link href="/">صفحه اصلی</Link>
           </li>
           <li>
-            <Link href="/residentials" className={styles.button}>
-              <IoDocumentsOutline />
-              <span>آگهی ها</span>
+            <Link
+              href="/residentials"
+              className="flex gap-1 border-2 border-third  py-1 px-2 rounded-xl"
+            >
+              <IoDocumentsOutline className="size-5" />
+              آگهی ها
             </Link>
           </li>
         </ul>
       </div>
-      <div className='flex'>
+      <div className="flex">
         {status === "loading" && <p>لطفا منتظر بمانید...</p>}
         {status === "authenticated" && (
-          <>
+          <div className="flex gap-1">
             {pathname !== "/dashboard" && (
-              <Link href="/dashboard" className={styles.button}>
-                <VscSymbolProperty />
+              <Link
+                href="/dashboard"
+                className="flex gap-1 border-2 border-third  py-1 px-2 rounded-xl"
+              >
+                <VscSymbolProperty className="size-5" />
               </Link>
             )}
-            <button onClick={signoutHandler} className={styles.button}>
-              <VscSignOut />
+            <button
+              onClick={signoutHandler}
+              className="flex gap-1 border-2 border-third  py-1 px-2 rounded-xl"
+            >
+              <VscSignOut className="size-5" />
               <span>خروج</span>
             </button>
-          </>
+          </div>
         )}
         {status === "unauthenticated" && (
           <>
             {pathname !== "/auth/login" && (
-              <Link href="/auth/login" className={styles.button}>
+              <Link
+                href="/auth/login"
+                className="flex gap-1 border-2 border-third  py-1 px-2 rounded-xl"
+              >
                 <VscSignIn />
                 <span>ورود</span>
               </Link>
             )}
             {pathname !== "/auth/register" && (
-              <Link href="/auth/register" className={styles.button}>
+              <Link
+                href="/auth/register"
+                className="flex gap-1 border-2 border-third  py-1 px-2 rounded-xl"
+              >
                 <GiArchiveRegister />
                 <span>ثبت نام</span>
               </Link>
